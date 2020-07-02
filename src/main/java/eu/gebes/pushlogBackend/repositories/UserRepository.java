cb153e4779fdea08b94c22558057604c31393fb6
Christoph Krassnigg <35232234+Gebes@users.noreply.github.com>
2020-07-02 07:36:53 +0200

    Create UserRepository.java

---
+++ src/main/java/eu/gebes/pushlogBackend/repositories/UserRepository.java
@@ -0,0 +1,6 @@
+package eu.gebes.pushlogBackend.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User, String> {
+}

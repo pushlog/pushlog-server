c66c44630b1e22c167da583fbe30c42531e7208f
Christoph Krassnigg <35232234+Gebes@users.noreply.github.com>
2020-07-02 14:51:49 +0200

    added get request

--- src/main/java/eu/gebes/pushlogBackend/routes/UserRoute.java
+++ src/main/java/eu/gebes/pushlogBackend/routes/UserRoute.java
@@ -4,6 +4,7 @@ import eu.gebes.pushlogBackend.components.TokenGenerator;
 import eu.gebes.pushlogBackend.repositories.User;
 import eu.gebes.pushlogBackend.repositories.UserRepository;
 import eu.gebes.pushlogBackend.response.BadRequestException;
+import eu.gebes.pushlogBackend.response.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,17 +22,27 @@ public class UserRoute {
     @Autowired
     TokenGenerator tokenGenerator;
 
+    @GetMapping("/user/{token}")
+    User getUserData(@PathVariable String token) {
+
+        User user = userRepository.findById(token).orElse(null);
+
+        if (user == null)
+            throw new NotFoundException("Couldn't find user with the requested token");
+
+        return user;
+    }
 
     @PostMapping("/user/")
-    User createUser(@RequestBody Map<String, String> body){
+    User createUser(@RequestBody Map<String, String> body) {
 
         final String displayname = body.get("displayname");
 
-        if(displayname == null)
+        if (displayname == null)
             throw new BadRequestException("A displayname parameter in the body is required");
 
 
-        if(displayname.length() > 32)
+        if (displayname.length() > 32)
             throw new BadRequestException("Displayname can't be greater than 32");
 
 

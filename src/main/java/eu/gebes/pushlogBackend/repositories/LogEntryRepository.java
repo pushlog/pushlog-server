03b0809c368ff80e957cae9b87c660b88cf85da4
Christoph Krassnigg <35232234+Gebes@users.noreply.github.com>
2020-07-03 08:32:50 +0200

    Update LogEntryRepository.java

--- src/main/java/eu/gebes/pushlogBackend/repositories/LogEntryRepository.java
+++ src/main/java/eu/gebes/pushlogBackend/repositories/LogEntryRepository.java
@@ -2,5 +2,9 @@ package eu.gebes.pushlogBackend.repositories;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface LogEntryRepository extends MongoRepository<LogEntry, String> {
+    Optional<List<LogEntry>> findLogEntriesByTimestampIsGreaterThanAndTimestampIsLessThan(Long start, Long end);
 }
